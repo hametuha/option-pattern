233b77ec7c891f85d183984d070d11ff79326fe7
fumikito <guy@hametuha.com>
2020-06-05 22:29:50 +0900

    Initial commit.

---
+++ bin/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+rm -rf .github
+rm -rf .gitignore
+rm -rf .gitattributes
+rm -rf .git
+rm -rf .wp-env.json
+rm -rf vendor
+rm -rf bin
+rm -rf tests
+rm -rf option-pattern.php
+rm -rf phpunit.xml.dist
---
+++ option-pattern.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: Option Pattern Library
+ * Plugin URI:  https://github.com/hametuha/option-pattern
+ * Description: Settings API helper.
+ * Version:     0.0.0
+ * Author:      Kunoichi INC.
+ * Author URI:  https://kunoichiwp.com
+ * License:     GPLv3 or later
+ * License URI: http://www.gnu.org/licenses/old-licenses/gpl-3.0.html
+ * Text Domain: option-pattern
+ * Domain Path: /languages
+ */
+
+// This file actually do nothing.
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'Invalid request.' );
+}
+require __DIR__ . '/vendor/autoload.php';
